73ec302f4ad053bdeb21fea5248b1012f704272b
ikeyler <119964453+ikeyler@users.noreply.github.com>
2026-03-21 17:42:34 +0300

    oops

--- src/main/src/main/java/ikeyler/mlmod/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package ikeyler.mlmod.commands;
-
-import java.util.List;
-
-public interface Command {
-    String getName();
-    void execute(List<String> args);
-    boolean isUtil(); // hide cmd in chat history
-}
